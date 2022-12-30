81252429be2135ae22cac3289f09b8b5a4dc1eac
Nikolai Glebov <glebov.nikolai91@gmail.com>
2022-12-30 17:44:57 +0300

    Проектная работа 4

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.popup__close-button');
+
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let nameInput = document.querySelector('.popup__input-name')
+let jobInput = document.querySelector('.popup__input-job')
+
+nameInput.value = profileName.textContent;
+jobInput.value = profileDescription.textContent;
+
+function popupToggle() {
+    popup.classList.toggle('popup_opened');
+}
+
+profileEditButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+    popupToggle()
+}
+
+let formElement = document.querySelector('.popup__form-edit')
+formElement.addEventListener('submit', handleFormSubmit);
